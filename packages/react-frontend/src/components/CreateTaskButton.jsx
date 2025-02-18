904b471bb7d6fbb5db23dd0c6a943dbff0ad9ba6
Galaxy334522 <alexsahli@outlook.com>
2025-02-18 09:52:24 -0800

    added HTML code for a 'Create Task' button; CSS will be next

---
+++ packages/react-frontend/src/components/CreateTaskButton.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// props for this button include color
+function CreateTaskButton(props) {
+  return <button> Create Task </button>;
+}
+
+export default CreateTaskButton;
