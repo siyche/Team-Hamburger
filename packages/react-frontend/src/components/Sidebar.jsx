7185cc28480fff505501d4f3e04776ad48fa79e7
Stephen Chen <stiyche23@hotmail.com>
2025-02-26 11:34:07 -0800

    Commit

--- packages/react-frontend/src/components/Sidebar.jsx
+++ packages/react-frontend/src/components/Sidebar.jsx
@@ -1,32 +1,13 @@
-import React from "react";
-import "../styles/Sidebar.css";
+import React from 'react';
+import '../styles/Sidebar.css';
 
-<link src="../styles/Sidebar.css" rel="stylesheet" />;
-
-// NOTE: all elements, even simple ones, include props as their color/theme can be changed
-
-// Create Task Button
-function CreateTaskButton(props) {
-  // props include colors
-  return <button class="MenuButton"> Create Task </button>;
-}
-
-// Monthly/Daily/Weekly View Button
-function ViewButton(props) {
-  // props include time (daily/monthly/weekly) and colors
-  return (
-    <button class="MenuButton">
-      {" "}
-      {props.time}
-      View{" "}
-    </button>
+const SideBar = () => {
+  // Sidebar component -> this will eventually hold the buttons for navigating
+    return (
+        <div className="sidebar">
+            <p>This will be the sidebar</p>
+        </div>
   );
-}
+};
 
-// Filter Menu
-function FilterMenu(props) {
-  // Should be a table with a header reading 'filter by type' and a body with a column of check boxes
-  // Some options exist by default; others can be created. All can be removed. There are more buttons at the bottom to do this.
-}
-
-export default CreateTaskButton;
+export default SideBar;
