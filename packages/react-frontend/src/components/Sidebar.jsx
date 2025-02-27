0bc5be699b6dd2b62e15abc4aaed5a6121da5d7f
Stephen Chen <stiyche23@hotmail.com>
2025-02-26 20:08:24 -0800

    Commit

--- packages/react-frontend/src/components/Sidebar.jsx
+++ packages/react-frontend/src/components/Sidebar.jsx
@@ -2,27 +2,11 @@ import React from 'react';
 import '../styles/Sidebar.css';
 
 const SideBar = () => {
-  return (
-    <div className="sidebar">
-      <button className="nav-button">Monthly View</button>
-      <button className="nav-button">Create Task</button>
-      
-      <div className="filter-section">
-        <h3>Filter By Type</h3>
-        <ul className="filter-list">
-          <button className="filter-item assignment">Assignment</button>
-          <button className="filter-item study">Study</button>
-          <button className="filter-item midterm">Midterm</button>
-          <button className="filter-item final">Final</button>
-          <button className="filter-item quiz">Quiz</button>
-          <button className="filter-item lecture">Lecture</button>
-          <button className="filter-item lab">Lab</button>
-          <button className="filter-item homework">Homework</button>
-          <button className="filter-item presentation">Presentation</button>
-          <button className="filter-item exam">Exam</button>
-        </ul>
-      </div>
-    </div>
+  // Sidebar component -> this will eventually hold the buttons for navigating
+    return (
+        <div className="sidebar">
+            <p>This will be the sidebar</p>
+        </div>
   );
 };
 
