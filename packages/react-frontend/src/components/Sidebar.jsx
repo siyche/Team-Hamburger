597953e69e2e9b2f606ff476ce183620bae17623
Stephen Chen <stiyche23@hotmail.com>
2025-02-26 19:15:53 -0800

    Commit

--- packages/react-frontend/src/components/Sidebar.jsx
+++ packages/react-frontend/src/components/Sidebar.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import CreateTaskButton from "./CreateTaskButton.jsx";
-import "../styles/Sidebar.css";
+import React from 'react';
+import '../styles/Sidebar.css';
 
 const SideBar = () => {
   // Sidebar component -> this will eventually hold the buttons for navigating
-  return (
-    <div className="sidebar">
-      <p>This will be the sidebar</p>
-      <CreateTaskButton />
-    </div>
+    return (
+        <div className="sidebar">
+            <p>This will be the sidebar</p>
+        </div>
   );
 };
 
