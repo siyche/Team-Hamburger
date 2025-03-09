920362a96192dc76639767d296da1dec5a9975d9
Galaxy334522 <alexsahli@outlook.com>
2025-03-09 16:44:57 -0700

    formatting

--- packages/react-frontend/src/components/DayCalendar.jsx
+++ packages/react-frontend/src/components/DayCalendar.jsx
@@ -48,6 +48,7 @@ const DayCalendarView = ({ onDaySelect }) => {
       {/* Header with month/year and navigation */}
       <div className="month-year-header">
         <span>
+          &nbsp;&nbsp; {/* extra space */}
           <img src="../hamburger.png" alt="Example Image" width="35" />
           &nbsp; {/* extra space */}
           {currentDate.toLocaleString("default", { weekday: "long" })}
