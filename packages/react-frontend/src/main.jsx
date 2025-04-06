afa521763ca278548f43ca0bcb8e824ad68d9250
Darryl <darryljamescruz@gmail.com>
2025-04-05 21:30:44 -0700

    implemented: frontend loginpage.jsx using taildwind css (maybe convert to an external .css for readability) -> need to do sign up page and code cleaning

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CalendarLayoutMonth from './pages/CalendarLayoutMonth';
 import CalendarLayoutWeek from './pages/CalendarLayoutWeek';
 import CalendarLayoutDay from './pages/CalendarLayoutDay';
+import Login from './pages/auth/LoginPage';
+import SignUp from './pages/auth/SignUpPage';
 import "./main.css"; // global styles if needed
 
 //TODO: WILL NEED TO BE REWORKED ONCE WE IMPLEMENT AUTHENTICATION TO INCLUDE PROTECTED ROUTING/LOGIN TOKEN
@@ -12,8 +14,12 @@ import "./main.css"; // global styles if needed
 import Settings from './components/Settings'; // ADD THIS IMPORT
 import "./main.css";
 
+// Main App component with routing
+// TODO: PROTECTED ROUTING
 const App = () => (
   <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<SignUp />} />
     <Route path="/month" element={<CalendarLayoutMonth />} />
     <Route path="/week" element={<CalendarLayoutWeek />} />
     <Route path="/day" element={<CalendarLayoutDay />} />
--- packages/react-frontend/src/pages/auth/LoginPage.jsx
+++ packages/react-frontend/src/pages/auth/LoginPage.jsx
@@ -12,12 +12,73 @@ export default function LoginPage() {
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <UsernamePasswordForm onSubmit={handleSubmit} />
-      <p>
-        Don't have an account? <Link to="/register">Register here</Link>
-      </p>
+    <div className ='flex min-h-full flex-1 flex-col justify-center px6 py-12 lg:px-8 font-mono'>
+      <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+        <img
+          alt='hamburger'
+          src='../../../public/hamburger.png'
+          className='mx-auto h-10 w-auto'
+        />
+        <h2 className='mt-10 text-center text-2xl/9 font-mono tracking-tight text-gray-900'>
+          Sign in to your account
+        </h2>
+      </div>
+
+      {/* Form for username and password */}
+      <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+        <form className='space-y-6'>
+          <div>
+            <label htmlFor='username' className='block text-sm/6 text-gray-700'>
+              Username
+            </label>
+            <div className='mt-2'>
+              <input
+                id='username'
+                name='username'
+                type='username'
+                autoComplete='username'
+                required
+                className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+            </div>
+          </div>
+
+          <div>
+            <div className='flex items-center justify-between'>
+              <label htmlFor='password' className='block text-sm/6 stext-gray-700'>
+                Password
+              </label>
+              </div>
+              <div className='mt-2'>
+                <input
+                  id='password'
+                  name='password'
+                  type='password'
+                  autoComplete='current-password'
+                  required
+                  className='block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6'
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type='submit'
+                className='flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600'
+              >
+                Sign in
+              </button>
+            </div>
+        </form>
+
+        <p className='mt-10 text-center text-sm/6 text-gray-500'>
+          Not a member?
+          <Link to='/register' className='font-semibold leading-6 text-blue-600 hover:text-blue-500'>
+            {' '}
+            Sign up
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
