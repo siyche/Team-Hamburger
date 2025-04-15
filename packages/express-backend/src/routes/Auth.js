30c60f5fcbf036be51268fe02fdac26fe508d24a
Darryl <darryljamescruz@gmail.com>
2025-04-15 10:07:44 -0700

    more changes

--- packages/express-backend/src/routes/Auth.js
+++ packages/express-backend/src/routes/Auth.js
@@ -6,7 +6,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import User from "../models/user.js";
-import Calendar from "../models/calendar.js";
+import Calendar from "../models/Calendar.js";
 
 const router = express.Router();
 router.use(cors());
