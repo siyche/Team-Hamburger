a41a2cf3659720b1714185e88b82033186e736b6
Galaxy334522 <alexsahli@outlook.com>
2025-04-29 16:32:57 -0700

    test

--- packages/react-frontend/eslint.config.js
+++ packages/react-frontend/eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
 import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
@@ -20,14 +19,12 @@ export default [
     settings: { react: { version: "detect" } },
     plugins: {
       react,
-      "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
-      ...reactHooks.configs.recommended.rules,
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
