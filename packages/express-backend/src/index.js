a5bb2b5472b51da45f9b7da19b70d9bf089d02c8
Darryl <darryljamescruz@gmail.com>
2025-05-01 08:25:40 -0700

    frontend can now be loaded from the backend
    call npm run dev from the backend

--- packages/express-backend/src/index.js
+++ packages/express-backend/src/index.js
@@ -2,25 +2,46 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
+import path from "path";
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+import connectDB from './config/db.js';
 import authRoutes from './routes/Auth.js';
 import eventRoutes from './routes/Events.js';
 
-dotenv.config();
+try {
+  const staticDir = path.join(__dirname, '../../react-frontend/dist');
+  console.log('Index.js will static files from:', staticDir);
+  dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+  const app = express();
 
-// connect to Mongo -> called in config/db.js
-await connectDB();
+  app.use(cors());
+  app.use(express.json());
 
-// mount routes
-app.use('/auth', authRoutes);
-app.use('/api/events', eventRoutes);
+  // connect to Mongo -> called in config/db.js
+  await connectDB();
+  
+  // mount routes
+  app.use('/auth', authRoutes);
+  app.use('/api/events', eventRoutes);
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+  // Serve static files from the React frontend app
+  app.use(express.static(staticDir));
+
+  // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(staticDir, "index.html"));
+  });
+
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+
+} catch (error) {
+  console.error("Error starting server:", error);
+  process.exit(1); // Exit the process with failure
+}
