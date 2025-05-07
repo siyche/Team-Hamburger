bbb363001833b0026ef95cbe5dd9b2a357c1ccd8
Galaxy334522 <alexsahli@outlook.com>
2025-05-06 20:09:26 -0700

    testing something

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -14,6 +14,7 @@ import "./main.css";
 
 // Main App component with routing
 const App = () => (
+  dhjfldshjflsdjhflsd
   <Routes>
     <Route path="/login" element={<Login />} />
     <Route path="/register" element={<SignUp />} />
