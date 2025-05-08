40c88e5f0f54a59add49c7badabc0c5bccbb24ae
Darryl <darryljamescruz@gmail.com>
2025-05-08 10:23:00 -0700

    modified static directory for deployemnt

--- packages/express-backend/src/index.js
+++ packages/express-backend/src/index.js
@@ -17,7 +17,8 @@ const app = express();
 try {
   // TODO: needs to be changed to the build folder
   //const staticDir = path.join(__dirname, '../../react-frontend/build');
-  const staticDir = path.join(__dirname, '../react-frontend/build');
+  //const staticDir = path.join(__dirname, '../react-frontend/build');
+  const staticDir = path.join(__dirname, 'build'); // not ../react-frontend/build
   console.log('Index.js will serve static files from:', staticDir);
 
   app.use(cors());
